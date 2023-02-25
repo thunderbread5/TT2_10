df95ed303b202e7df9787f31e5f823c54586c59f
Madeleine Hoo <jlhoo.2019@sis.smu.edu.sg>
2023-02-25 14:37:18 +0800

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcryptjs");
 const User = require("../models/userModel");
+const Policy = require("../models/policyModel")
 const Claim = require("../models/claimModel");
 const jwt = require("jsonwebtoken");
 const config = require("../config/auth.config");
@@ -37,8 +38,8 @@ const registerUser = asyncHandler(async (req, res) => {
 
     if (user) {
         res.status(201).json({
-            _id: user._id,
             EmployeeID: user.EmployeeID,
+            Password: user.Password,
             FirstName: user.FirstName,
             LastName: user.LastName,
             Age: user.Age,
@@ -91,6 +92,56 @@ const generateToken = (id) => {
     });
 };
 
+const addPolicy = asyncHandler(async (req, res) => {
+    const { InsuranceID, EmployeeID, InsuranceType, PolicyStartDate, PolicyTerm, 
+        PolicyEndDate, ClaimLimit, RemainingClaimLimit } = req.body;
+        if (!InsuranceID || !EmployeeID || !InsuranceType ) {
+            res.status(400);
+            throw new Error("Please include all fields");
+        }
+    const user = await User.findOne({ EmployeeID })
+    if (!user) {
+        res.status(400);
+        throw new Error("Employee not found");
+    }
+    const policy = await Policy.create({
+        InsuranceID,
+        EmployeeID,
+        InsuranceType, 
+        PolicyStartDate, 
+        PolicyTerm, 
+        PolicyEndDate, 
+        ClaimLimit, 
+        RemainingClaimLimit
+    })
+    if (policy) {
+        res.status(201).json({
+            InsuranceID: policy.InsuranceID,
+            EmployeeID: policy.EmployeeID,
+            InsuranceType: policy.InsuranceType,
+            PolicyStartDate: policy.PolicyStartDate,
+            PolicyTerm: policy.PolicyTerm,
+            PolicyEndDate: policy.PolicyEndDate,
+            ClaimLimit: policy.ClaimLimit,
+            RemainingClaimLimit: policy.RemainingClaimLimit,
+        });
+    } else {
+        res.status(400);
+        throw new Error("Invalid policy data");
+    }
+})
+
+const getMyPolicies = asyncHandler(async (req, res) => {
+    employee_id = req.user.EmployeeID
+    const policies = await Policy.find({ "EmployeeID": employee_id })
+    if (policies.length > 0) {
+        res.status(200).json(policies);
+    } else {
+        res.status(404);
+        throw new Error("Employee has no policies");
+    }
+    });
+
 const addClaim = asyncHandler(async (req, res) => {
     const { EmployeeID, ClaimID, InsuranceID, FirstName, LastName, ExpenseDate, Amount, Purpose,
         FollowUp, PreviousClaimID, Status, LastEditedClaimDate } = req.body;
@@ -142,12 +193,10 @@ const addClaim = asyncHandler(async (req, res) => {
     }
 });
 
-
-
-
 module.exports = {
     registerUser,
     loginUser,
     getMe,
+    addPolicy,
     addClaim,
 };
