2ce26236302c264c615e708cda2a11caf4b9e4eb
Christopher-LM <>
2023-02-25 10:34:53 +0800

    Use mongodb online

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,20 @@
 const express = require("express");
-const { errorHandler } = require("./middleware/errorMiddleware");
-const connectDB = require("./db.js");
-const dotenv = require("dotenv").config();
-const PORT = process.env.PORT || 5000;
-
-connectDB();
-
+const mongoose = require('mongoose');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const uri = "mongodb+srv://john088:A1122334455A@cluster0.knubn2m.mongodb.net/?retryWrites=true&w=majority";
 const app = express();
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     next();
-// });
-
-app.get("/", (req, res) => {
-    res.status(200).json({ messsage: "Welcome" });
-});
+app.use(cors());
+app.use("/api/users", router);
+app.use("/api/transactions", tranRouter);
 
 app.use("/api/users", require("./route/userRoutes"));
-app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+mongoose
+    .set('strictQuery', true)
+    .connect(uri)
+    .then(() =>
+        app.listen(port, '0.0.0.0', () => {console.log(`Server running on port ${port}`)}))
+    .catch((err) => console.log(err));
