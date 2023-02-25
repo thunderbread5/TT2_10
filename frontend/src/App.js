e09c813f46a13fec19ecf45435a0df22aa7a7805
jing sheng <teyjingsheng@gmail.com>
2023-02-25 13:35:22 +0800

    adding routing for new claim

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Header from "./components/Header";
 import { UserProvider } from "./context/user/UserContext";
+import NewClaim from "./pages/NewClaim";
 
 function App() {
 
@@ -20,6 +21,7 @@ function App() {
                             <Route path="/" element={<Home />} />
                             <Route path="/login" element={<Login />} />
                             <Route path="/register" element={<Register />} />
+                            <Route path="/newclaim" element={<NewClaim />} />
                         </Routes>
                     </div>
                 </Router>
