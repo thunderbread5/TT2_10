8eac6c1b361f67b7a1829216026915c1ad9ebaa0
Madeleine Hoo <jlhoo.2019@sis.smu.edu.sg>
2023-02-25 15:28:48 +0800

    update policies (add, get by employeeid)

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -10,7 +10,7 @@ const config = require("../config/auth.config");
 // @route   /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
-    const { EmployeeID, FirstName, Password, LastName, Age} = req.body;
+    const { EmployeeID, FirstName, Password, LastName, Age } = req.body;
     if (!FirstName || !Password || !LastName || !Age) {
         res.status(400);
         throw new Error("Please include all fields");
@@ -77,10 +77,11 @@ const loginUser = asyncHandler(async (req, res) => {
 // @access  Private
 const getMe = asyncHandler(async (req, res) => {
     const user = {
-        id: req.user._id,
         EmployeeID: req.user.EmployeeID,
         FirstName: req.user.FirstName,
+        LastName: req.user.LastName,
         Password: req.user.Password,
+        Age: req.user.Age
     };
     res.status(200).json(user);
 });
@@ -92,54 +93,57 @@ const generateToken = (id) => {
 };
 
 const addPolicy = asyncHandler(async (req, res) => {
-    const { InsuranceID, EmployeeID, InsuranceType, PolicyStartDate, PolicyTerm, 
+    const { InsuranceID, EmployeeID, InsuranceType, PolicyStartDate, PolicyTerm,
         PolicyEndDate, ClaimLimit, RemainingClaimLimit } = req.body;
-        if (!InsuranceID || !EmployeeID || !InsuranceType ) {
-            res.status(400);
-            throw new Error("Please include all fields");
-        }
+    if (!InsuranceID || !EmployeeID || !InsuranceType) {
+        res.status(400);
+        throw new Error("Please include all fields");
+    }
     const user = await User.findOne({ EmployeeID })
     if (!user) {
         res.status(400);
         throw new Error("Employee not found");
     }
     const policy = await Policy.create({
-        InsuranceID,
-        EmployeeID,
-        InsuranceType, 
-        PolicyStartDate, 
-        PolicyTerm, 
-        PolicyEndDate, 
-        ClaimLimit, 
-        RemainingClaimLimit
+        InsuranceID: InsuranceID,
+        EmployeeID: EmployeeID,
+        InsuranceType: InsuranceType,
+        PolicyStartDate: PolicyStartDate,
+        PolicyTerm: PolicyTerm,
+        PolicyEndDate: PolicyEndDate,
+        ClaimLimit: ClaimLimit,
+        RemainingClaimLimit: RemainingClaimLimit
     })
     if (policy) {
-        res.status(201).json({
-            InsuranceID: policy.InsuranceID,
-            EmployeeID: policy.EmployeeID,
-            InsuranceType: policy.InsuranceType,
-            PolicyStartDate: policy.PolicyStartDate,
-            PolicyTerm: policy.PolicyTerm,
-            PolicyEndDate: policy.PolicyEndDate,
-            ClaimLimit: policy.ClaimLimit,
-            RemainingClaimLimit: policy.RemainingClaimLimit,
-        });
+        res.status(201).json({ policy });
     } else {
         res.status(400);
         throw new Error("Invalid policy data");
     }
 })
 
-const getMyPolicies = asyncHandler(async (req, res) => {
-    employee_id = req.user.EmployeeID
-    const policies = await Policy.find({ "EmployeeID": employee_id })
-    if (policies.length > 0) {
-        res.status(200).json(policies);
-    } else {
-        res.status(404);
-        throw new Error("Employee has no policies");
+const getPolicies = asyncHandler(async (req, res) => {
+    const { EmployeeID } = req.body;
+    if (!EmployeeID) {
+        res.status(400);
+        throw new Error("Please include EmployeeID");
     }
+
+    // Find if user exists
+    const userExists = await User.findOne({ EmployeeID });
+    if (!userExists) {
+        res.status(400);
+        throw new Error("User not found");
+    }
+
+    // get all policies
+    let policies;
+    policies = await Policy.find({ EmployeeID: EmployeeID });
+    return res.status(200).json({
+        policies
     });
+})
+
 
 const addClaim = asyncHandler(async (req, res) => {
     const { EmployeeID, InsuranceID, FirstName, LastName, ExpenseDate, Amount, Purpose,
@@ -194,7 +198,7 @@ const getClaims = asyncHandler(async (req, res) => {
 
     // get all claims
     let claims;
-    claims = await Claim.find({employeeId: employeeId});
+    claims = await Claim.find({ employeeId: employeeId });
     return res.status(200).json({ claims });
 });
 
@@ -211,8 +215,8 @@ const deleteClaim = asyncHandler(async (req, res) => {
         res.status(400);
         throw new Error("Claim not found");
     }
-    claim.remove({claimId: claimId});
-    return res.status(200).json({ message: "Successful delete of claimId:" +  claimId});
+    claim.remove({ claimId: claimId });
+    return res.status(200).json({ message: "Successful delete of claimId:" + claimId });
 
     // get all claims
 
@@ -223,6 +227,7 @@ module.exports = {
     loginUser,
     getMe,
     addPolicy,
+    getPolicies,
     addClaim,
     getClaims,
     deleteClaim,
