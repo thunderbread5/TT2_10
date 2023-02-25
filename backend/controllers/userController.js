23223219685ecd8551a655d6d4e75353a2b6f268
Christopher-LM <>
2023-02-25 11:39:21 +0800

    add login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,13 +2,14 @@ const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcryptjs");
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
+const config = require("../config/auth.config");
 
 // @desc    Register a new user
 // @route   /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
-    const { name, email, password } = req.body;
-    if (!name || !email || !password) {
+    const { employeeId, name, email, password, lastName} = req.body;
+    if (!name || !email || !password || !lastName) {
         res.status(400);
         throw new Error("Please include all fields");
     }
@@ -26,7 +27,9 @@ const registerUser = asyncHandler(async (req, res) => {
 
     // Create user
     const user = await User.create({
+        employeeId,
         name,
+        lastName,
         email,
         password: hashedPassword,
     });
@@ -34,9 +37,10 @@ const registerUser = asyncHandler(async (req, res) => {
     if (user) {
         res.status(201).json({
             _id: user._id,
+            employeeId: user.employeeId,
             name: user.name,
+            lastName: user.lastName,
             email: user.email,
-            token: generateToken(user._id),
         });
     } else {
         res.status(400);
@@ -48,16 +52,18 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   /api/users/login
 // @access  Public
 const loginUser = asyncHandler(async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
+    const { employeeId, password } = req.body;
+    const user = await User.findOne({ employeeId });
 
     // check user and password match
     if (user && (await bcrypt.compare(password, user.password))) {
         res.status(200).json({
             _id: user._id,
+            employeeId: user.employeeId,
             name: user.name,
+            lastName: user.lastName,
             email: user.email,
-            token: generateToken(user._id),
+            token: generateToken(user.employeeId),
         });
     } else {
         res.status(401);
@@ -71,14 +77,16 @@ const loginUser = asyncHandler(async (req, res) => {
 const getMe = asyncHandler(async (req, res) => {
     const user = {
         id: req.user._id,
+        employeeId: req.user.employeeId,
         email: req.user.email,
         name: req.user.name,
+        password: req.user.password,
     };
     res.status(200).json(user);
 });
 
 const generateToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, {
+    return jwt.sign({ id }, config.secret, {
         expiresIn: "30d",
     });
 };
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,9 +2,17 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema(
     {
+        employeeId: {
+            type: String,
+            required: [true, "Add employee ID please"],
+        },
         name: {
             type: String,
             required: [true, "Add name please"],
+        },
+        lastName: {
+            type: String,
+            required: [true, "Add last name please"],
         },
         email: {
             type: String,
@@ -14,15 +22,7 @@ const userSchema = mongoose.Schema(
         password: {
             type: String,
             required: [true, "Add password please"],
-        },
-        isAdmin: {
-            type: Boolean,
-            required: true,
-            default: false,
-        },
-    },
-    {
-        timestamps: true,
+        }
     }
 );
 
