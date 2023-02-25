0c6c27f0333d484593266b4a22c21287a4dd55da
Christopher-LM <>
2023-02-25 16:13:48 +0800

    change func to post

--- backend/route/userRoutes.js
+++ backend/route/userRoutes.js
@@ -13,7 +13,7 @@ router.post("/", registerUser);
 router.post("/login", loginUser);
 router.get("/me", protect, getMe);
 router.post("/claims", protect, addClaim);
-router.get("/claims", protect, getClaims);
+router.post("/claims/getAll", protect, getClaims);
 router.post("/claims/delete", protect, deleteClaim);
 router.put("/claims/update", protect, updateClaim);
 
