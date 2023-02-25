dcc08d4412b980a8089d5c1e0cf35c461b829b79
hidemioura <hidemi2808@gmail.com>
2023-02-25 14:38:39 +0800

    View Claims from accounts to Last Edit claim date

--- frontend/src/pages/View.jsx
+++ frontend/src/pages/View.jsx
@@ -29,13 +29,60 @@ function ViewClaim() {
             id="date"
             name="date"
           ></input>
+          <label htmlFor="Amount"> Amount </label>
+          <input
+            type="int"
+            id="purpose"
+            name="purpose"
+            value={0}
+            disabled={""} //for view function
+          ></input>
           <label htmlFor="purpose">Purpose of Expenditure: </label>
           <input
             type="text"
             placeholder="Enter your purpose here"
             id="purpose"
             name="purpose"
+            value={0}
+            disabled={""} //for view function
           ></input>
+          <label htmlFor="followup">FollowUp </label>
+          <input
+            type="text"
+            placeholder="Follow Up"
+            id="followup"
+            name="followup"
+            value={0}
+            disabled={""} //for view function
+          ></input>
+          <label htmlFor="PreviousClaimId">Previous Claim ID </label>
+          <input
+            type="text"
+            placeholder="PreviousClaimId"
+            id="PreviousClaimId"
+            name="PreviousClaimId"
+            value={0}
+            disabled={""} //for view function
+          ></input>
+          <label htmlFor="Status">Status </label>
+          <input
+            type="text"
+            placeholder="Status"
+            id="status"
+            name="status"
+            value={'Approved'}
+            disabled={"Approved"} //for view function
+          ></input>
+          <label htmlFor="LastEditClaimDate">Last Editted Date </label>
+          <input
+            type="text"
+            placeholder="Status"
+            id="status"
+            name="status"
+            value={'0'}
+            disabled={""} //for view function
+          ></input>
+          
         </div>
         <button onClick={""} className="btn btn-block">
           Create Claim
